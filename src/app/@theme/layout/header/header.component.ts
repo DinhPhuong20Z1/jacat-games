0e5af4cb6a98e07eab22b2953e1d89b1032041dc
phuong1772001 <phuongdd@volio.vn>
2022-07-19 17:26:09 +0700

    update

--- src/app/@theme/layout/header/header.component.ts
+++ src/app/@theme/layout/header/header.component.ts
@@ -12,4 +12,21 @@ export class HeaderComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  myFunction() {
+    var x = document.getElementById('menu-mobile');
+    if (x != null) {
+      if (x.style.display === 'block') {
+        // if (x.className === "topnav") {
+        //   x.className += " responsive";
+        // } else {
+        //   x.className = "topnav";
+        // }
+        x.style.display = 'none';
+      } else {
+        x.style.display = 'block';
+      }
+    }
+
+  }
+
 }
